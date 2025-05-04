93db17b706886a154887e90b1d1c4d84b92a34a6
Cats_have7lives <165317902+CatsHave7lives@users.noreply.github.com>
2025-05-04 04:48:03 +0100

    echo completed

--- ms_builtins/ms_echo.c
+++ ms_builtins/ms_echo.c
@@ -6,50 +6,75 @@
 /*   By: aessaber <aessaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 21:42:01 by aessaber          #+#    #+#             */
-/*   Updated: 2025/05/03 23:22:02 by aessaber         ###   ########.fr       */
+/*   Updated: 2025/05/04 04:44:01 by aessaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static bool	echo_flag_check(int ac, char **av, size_t *row)
-{
-	size_t	col;
-	bool	check;
+// static bool	echo_flag_check(int ac, char **av, size_t *row)
+// {
+// 	size_t	col;
+// 	bool	check;
 
-	*row = 1;
-	check = false;
-	while (*row < ac && av[*row][0] == '-' && av[*row][1] == 'n')
-	{
-		col = 2;
-		while (av[*row][col] == 'n')
-			col++;
-		if (av[*row][col] == '\0')
-			check = true;
-		else
-			break ;
-		(*row)++;
-	}
-	return (check);
-}
+// 	*row = 1;
+// 	check = false;
+// 	while (*row < ac && av[*row][0] == '-' && av[*row][1] == 'n')
+// 	{
+// 		col = 2;
+// 		while (av[*row][col] == 'n')
+// 			col++;
+// 		if (av[*row][col] == '\0')
+// 			check = true;
+// 		else
+// 			break ;
+// 		(*row)++;
+// 	}
+// 	return (check);
+// }
 
-int	ms_echo(int ac, char **av)
+// int	ms_echo(int ac, char **av)
+// {
+// 	size_t	row;
+// 	bool	flag_n;
+// 	bool	is_first;
+
+// 	flag_n = echo_flag_check(ac, av, &row);
+// 	is_first = true;
+// 	while (row < ac)
+// 	{
+// 		if (!is_first)
+// 			ft_putchar(' ');
+// 		ft_putstr(av[row]);
+// 		is_first = false;
+// 		row++;
+// 	}
+// 	if (!flag_n)
+// 		ft_putchar('\n');
+// 	return (0);
+// }
+
+int	ms_echo(t_arg *arg)
 {
-	size_t	row;
 	bool	flag_n;
 	bool	is_first;
 
-	flag_n = echo_flag_check(ac, av, &row);
+	flag_n = false;
+	while (arg && arg->str[0] == '-' && ms_arg_is_constant(arg->str, 'n', 1))
+	{
+		flag_n = true;
+		arg = arg->next;
+	}
 	is_first = true;
-	while (row < ac)
+	while (arg)
 	{
 		if (!is_first)
 			ft_putchar(' ');
-		ft_putstr(av[row]);
+		ft_putstr(arg->str);
 		is_first = false;
-		row++;
+		arg = arg->next;
 	}
 	if (!flag_n)
 		ft_putchar('\n');
-	return (0);
+	return (EXIT_SUCCESS);
 }
--- ms_library/ms_arg_is_constant.c
+++ ms_library/ms_arg_is_constant.c
@@ -1,36 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   ms_is_constant.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aessaber <aessaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/01 21:42:42 by aessaber          #+#    #+#             */
-/*   Updated: 2025/05/04 04:44:28 by aessaber         ###   ########.fr       */
+/*   Created: 2025/05/04 02:15:39 by aessaber          #+#    #+#             */
+/*   Updated: 2025/05/04 02:51:31 by aessaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "../minishell.h"
 
-# include <stdbool.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-# define FD_OP 1
-# define SUCCESS 0
-
-typedef struct s_arg
+bool	ms_arg_is_constant(char *str, char constant, size_t start)
 {
-	char			*str;
-	struct s_arg	*next;
-}					t_arg;
-
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-
-int		ms_echo(t_arg *arg);
-
-bool	ms_arg_is_constant(char *str, char constant, size_t start);
-
-#endif
+	while (str[start])
+	{
+		if (str[start] != constant)
+			return (false);
+		start++;
+	}
+	return (true);
+}
